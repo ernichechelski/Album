79bafbbdaf13325ef05312daa73127529adc1a76
Ernest Chechelski <ernichechelski@gmail.com>
2019-04-13 12:42:55 +0200

    Initial Commit

---
+++ Album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by Ernest Chechelski on 13/04/2019.
+//  Copyright © 2019 Ernest Chechelski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
